ad80ba0693615d42113ef38a50e57079c33d9075
Gagandeep Thapar <gsthapar@gmail.com>
2023-07-22 01:00:43 -0700

    renamed consts file to prevent ifndef collisions

---
+++ sim_constants.hpp
@@ -0,0 +1,8 @@
+#ifndef SIM_CONSTANTS_HPP
+#define SIM_CONSTANTS_HPP
+
+namespace SIM_CONST {
+const double DT = 0.1;
+const double SMALL = 1e-10;
+};     // namespace SIM_CONST
+#endif // !SIM_CONSTANTS_HPP
